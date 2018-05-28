c02343049753359eb78bd73c5bb805242d107742
AaditT <33333433+AaditT@users.noreply.github.com>
2018-05-28 14:58:44 -0700

    Update mello-vol3.py

--- mello-vol3.py
+++ mello-vol3.py
@@ -1,6 +1,3 @@
-# mello.py - Mello.
-# Written by Srihari Nanniyur and Aadit Trivedi
-
 from random import shuffle
 import os
 import RPi.GPIO as GPIO
@@ -61,8 +58,66 @@ wav_files = {
     "Ab": "Gsharp.wav",
     "G#": "Gsharp.wav"
 }
+def get_vol():
+    clk = 17
+    dt = 18
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    rcounter = 0
+    clkLastState = GPIO.input(clk)
+    try:
+        # might have to escape this infinite loop
+        while True:
+            clkState = GPIO.input(clk)
+		    dtState = GPIO.input(dt)
+		    if clkState != clkLastState:
+			    if dtState != clkState: rcounter += 1
+			    else: rcounter -= 1
+			    print(rcounter)
+		    return(convert(rcounter))
+			clkLastState = clkState
+			time.sleep(0.01)
+	finally:
+		GPIO.cleanup()
+def convert(rotarycounter):
+    # default state
+    if rotarycounter == 0: return(1)
+    # increasing volume
+    if rotarycounter == 1: return(1.1)
+    if rotarycounter == 2: return(1.2)
+    if rotarycounter == 3: return(1.3)
+    if rotarycounter == 4: return(1.4)
+    if rotarycounter == 5: return(1.5)
+    if rotarycounter == 6: return(1.6)
+    if rotarycounter == 7: return(1.7)
+    if rotarycounter == 8: return(1.8)
+    if rotarycounter == 9: return(1.9)
+    if rotarycounter == 10: return(2)
+    if rotarycounter == 11: return(2.1)
+    if rotarycounter == 12: return(2.2)
+    if rotarycounter == 13: return(2.3)
+    if rotarycounter == 14: return(2.4)
+    if rotarycounter == 15: return(2.5)
+    if rotarycounter == 16: return(2.6)
+    if rotarycounter == 17: return(2.7)
+    if rotarycounter == 18: return(2.8)
+    if rotarycounter == 19: return(2.9)
+    if rotarycounter == 20: return(3)
+    # decreasing volume
+    if rotarycounter == -1: return(0.9)
+    if rotarycounter == -2: return(0.8)
+    if rotarycounter == -3: return(0.7)
+    if rotarycounter == -3: return(0.6)
+    if rotarycounter == -4: return(0.5)
+    if rotarycounter == -5: return(0.4)
+    if rotarycounter == -6: return(0.3)
+    if rotarycounter == -7: return(0.2)
+    if rotarycounter >= -8: return(0.1)
 
 def play_notes(notes, length, c):
+    vol = "-v "
+
     cmd = c + " "
     directory = ""
     if length == "short":
@@ -74,12 +129,15 @@ def play_notes(notes, length, c):
     else:
         raise Exception("play_note(): Invalid length!")
     for note in notes:
-        cmd += directory + wav_files[note] + " "
+        cmd += vol + str(convert(get_vol())) + " " + directory + wav_files[note] + " "
     print(cmd)
     os.system(cmd)
 
 def analyze_face():
     pass
+
+
+
 
 button = False
 
