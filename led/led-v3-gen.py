4852be32b78457a2e8440ff782805c43c8df3d3a
AaditT <33333433+AaditT@users.noreply.github.com>
2018-05-29 00:28:07 +0000

    Update led-v3-gen.py

--- led/led-v3-gen.py
+++ led/led-v3-gen.py
@@ -26,8 +26,22 @@ def ind_blink(pin):
       GPIO.output(13, GPIO.LOW)
       GPIO.output(15, GPIO.HIGH)
   else: print("blink(): Invalid pin")
-
+    
+def mult_blink(pin1, pin2):
+  GPIO.output(11, GPIO.LOW)
+  GPIO.output(13, GPIO.LOW)
+  GPIO.output(15, GPIO.LOW)
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(pin1, GPIO.OUT)
+  GPIO.setup(pin2, GPIO.OUT)
+  GPIO.output(pin1, GPIO.HIGH)
+  GPIO.output(pin2, GPIO.HIGH)
+    
 
 def red(): ind_blink (11)
 def green(): ind_blink (13)
 def blue(): ind_blink (15)
+  
+def yellow(): mult_blink(11,13)
+def cyan(): mult_blink(13,15)
+def magenta(): mult_blink(11,15)
