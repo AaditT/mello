ce2fff6fc6619009f70b8a1de914ee4f497f83aa
AaditT <33333433+AaditT@users.noreply.github.com>
2018-05-28 17:12:04 -0700

    Update led-v2-mult.py

--- led/led-v2-mult.py
+++ led/led-v2-mult.py
@@ -7,23 +7,18 @@ GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 
 
-def ind_blink(pin):
-  if (pin == 11) or (pin == 13) or (pin == 15):
-    if (pin == 11):
-      GPIO.output(11, GPIO.HIGH)
-      GPIO.output(13, GPIO.LOW)
-      GPIO.output(15, GPIO.LOW)
-    elif (pin == 13):
-      GPIO.output(11, GPIO.LOW)
-      GPIO.output(13, GPIO.HIGH)
-      GPIO.output(15, GPIO.LOW)
-    elif (pin == 15):
-      GPIO.output(11, GPIO.LOW)
-      GPIO.output(13, GPIO.LOW)
-      GPIO.output(15, GPIO.HIGH)
-  else: print("blink(): Invalid pin")
-
+def mult_blink(pin1, pin2):
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(pin1, GPIO.OUT)
+  GPIO.setup(pin2, GPIO.OUT)
+  GPIO.output(pin1, GPIO.HIGH)
+  GPIO.output(pin2, GPIO.LOW)
+    
 
 def red(): ind_blink (11)
 def green(): ind_blink (13)
 def blue(): ind_blink (15)
+  
+def yellow(): mult_blink(11,13)
+def cyan(): mult_blink(13,15)
+def magenta(): mult_blink(11,15)
