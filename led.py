1f54558e425376e1f4fdb0f383602d40e633e818
AaditT <33333433+AaditT@users.noreply.github.com>
2018-05-29 17:35:07 -0700

    Update led.py

--- led.py
+++ led.py
@@ -5,10 +5,6 @@ import RPi.GPIO as GPIO
 import os, sys
 from random import randint
 
-short_len = 0.25
-medium_len = 0.75
-long_len = 1.5
-
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
